c311dd05133db0a14cb76d200e34e3e6da7c6dfe
Sunil Prasath <tce.sunilprasath@gmail.com>
2023-01-30 17:26:22 +0530

    Add files via upload

---
+++ smorphi_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sunil/smorphi_ws/src
+# Build directory: /home/sunil/smorphi_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rplidar_ros")
